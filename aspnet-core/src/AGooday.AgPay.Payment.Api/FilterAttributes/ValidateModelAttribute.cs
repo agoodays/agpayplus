45ab69fe5b3eaf118af7ca34425842dc5b915cc5
xiangyisheng <1697504283@qq.com>
2022-09-07 18:11:19 +0800

    框架搭建

---
+++ aspnet-core/src/AGooday.AgPay.Payment.Api/FilterAttributes/ValidateModelAttribute.cs
@@ -0,0 +1,34 @@
+﻿using AGooday.AgPay.Payment.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace AGooday.AgPay.Payment.Api.FilterAttributes
+{
+    public class ValidateModelAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            // 如果其他过滤器已经设置了结果，则跳过验证
+            if (context.Result != null) return;
+
+            // 如果验证通过，跳过后面的动作
+            if (context.ModelState.IsValid) return;
+
+            // 获取失败的验证信息列表
+            var errors = new Dictionary<string, object>();
+            context.ModelState
+                .Where(s => s.Value != null && s.Value.ValidationState == ModelValidationState.Invalid)
+                .ToList().ForEach((item) =>
+                {
+                    errors.Add(item.Key, item.Value!.Errors.Select(e => e.ErrorMessage).ToList());
+                });
+
+            // 统一返回格式
+            var result = new ApiRes(StatusCodes.Status400BadRequest, "数据验证不通过！", errors, string.Empty);
+
+            // 设置结果
+            context.Result = new BadRequestObjectResult(result);
+        }
+    }
+}
--- aspnet-core/src/AGooday.AgPay.Payment.Api/Program.cs
+++ aspnet-core/src/AGooday.AgPay.Payment.Api/Program.cs
@@ -1,3 +1,8 @@
+using AGooday.AgPay.Payment.Api.FilterAttributes;
+using AGooday.AgPay.Payment.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    // 禁用默认模型验证过滤器
+    options.SuppressModelStateInvalidFilter = true;
+});
+builder.Services.Configure<MvcOptions>(options =>
+{
+    // 全局添加自定义模型验证过滤器
+    options.Filters.Add<ValidateModelAttribute>();
+});
 
 var app = builder.Build();
 
