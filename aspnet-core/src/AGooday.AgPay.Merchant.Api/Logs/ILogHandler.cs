a381c1c401874ca827322e42b1b42148f9de5951
Xiang YiSheng <1697504283@qq.com>
2022-11-23 23:26:14 +0800

    MCH操作日志与JWT

---
+++ aspnet-core/src/AGooday.AgPay.Merchant.Api/Logs/ILogHandler.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AGooday.AgPay.Merchant.Api.Logs
+{
+    /// <summary>
+    /// 操作日志处理接口
+    /// </summary>
+    public interface ILogHandler
+    {
+        /// <summary>
+        /// 写操作日志
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="next"></param>
+        /// <returns></returns>
+        Task LogAsync(ActionExecutingContext context, ActionExecutionDelegate next);
+    }
+}
