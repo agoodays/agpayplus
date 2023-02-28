721e665707e77edbb27f9cd6e244b0e9bbce92f2
xiangyisheng <1697504283@qq.com>
2023-02-28 13:13:24 +0800

    修改日期选择控件

--- ant-design-vue/agpay-ui-manager/src/components/AgDateRangePicker/AgDateRangePicker.vue
+++ ant-design-vue/agpay-ui-manager/src/components/AgDateRangePicker/AgDateRangePicker.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <a-select
-        v-if="optionValue!=='customDateTime'"
-        v-model="optionValue"
-        placeholder=""
-        @change="optionChange"
-        style="width: 100%"
-        ref="dateSelect"
+      v-if="optionValue!=='customDateTime'"
+      v-model="optionValue"
+      placeholder=""
+      @change="optionChange"
+      style="width: 100%"
+      ref="dateSelect"
     >
       <a-select-option v-for="o in options" :value="o.value" :key="o.value">
         {{ o.name }}
@@ -16,6 +16,7 @@
       placement="bottom"
       trigger="hover"
       :visible="dateRangeTipIsShow"
+      @visibleChange="handleHoverChange"
     >
       <template #content>
         <span style="white-space:nowrap;">{{ dateRangeTip }}</span>
@@ -23,18 +24,24 @@
       <a-range-picker
         v-if="optionValue==='customDateTime'"
         @change="onChange"
-        @mouseenter="onMouseenter"
-        @mouseleave="onMouseleave"
         style="width: 100%"
         ref="dateRangePicker"
+        :open="dateRangeOpen"
+        @openChange="handleDateRangeOpenChange"
+        :format="format"
+        :ranges="{
+          // 今天: [moment().startOf('day'), moment()],
+          // 昨天: [moment().startOf('day').subtract(1,'days'), moment().endOf('day').subtract(1, 'days')],
+          // 最近三天: [moment().startOf('day').subtract(2, 'days'), moment().endOf('day')],
+          // 最近一周: [moment().startOf('day').subtract(1, 'weeks'), moment()],
+          本月: [moment().startOf('month'), moment()],
+          本年: [moment().startOf('year'), moment()]
+        }"
       >
         <a-icon slot="suffixIcon" type="sync"/>
-        <template #renderExtraFooter>
-          <span style="cursor: pointer;" @click="onClick">
-            <span role="img" aria-label="left-circle" class="anticon anticon-left-circle">
-              <a-icon type="left-circle" />
-            </span>
-            返回日期下拉框
+        <template #renderExtraFooter style="float: right;">
+          <span class="ant-tag ant-tag-blue" style="cursor: pointer;" @click="onClick">
+            <a-icon type="left-circle" /> 返回日期下拉框
           </span>
         </template>
       </a-range-picker>
@@ -43,10 +50,13 @@
 </template>
 
 <script>
+import { ref } from 'vue'
+import moment from 'moment'
 export default {
   name: 'AgDateRangePicker',
   props: {
     value: { type: String, default: '' },
+    format: { type: String, default: 'YYYY-MM-DD' },
     options: {
       type: Array,
       default: () => ([
@@ -60,11 +70,27 @@ export default {
     }
   },
   data () {
+    const dateRangeTip = ''
+    const dateRangeTipIsShow = ref(false)
+    const handleHoverChange = visible => {
+      if (this.dateRangeTip.length > 0) {
+        dateRangeTipIsShow.value = visible
+      } else {
+        dateRangeTipIsShow.value = false
+      }
+    }
+    const dateRangeOpen = ref(false)
+    const handleDateRangeOpenChange = open => {
+      dateRangeOpen.value = open
+    }
     return {
       optionValue: this.value,
       optionOriginValue: this.value,
-      dateRangeTipIsShow: false,
-      dateRangeTip: ''
+      dateRangeTip,
+      dateRangeTipIsShow,
+      handleHoverChange,
+      dateRangeOpen,
+      handleDateRangeOpenChange
     }
   },
   methods: {
@@ -72,8 +98,11 @@ export default {
       if (this.optionValue !== 'customDateTime') {
         this.optionOriginValue = this.optionValue
         this.$emit('change', this.optionValue)
+      } else {
+        this.handleDateRangeOpenChange(true)
       }
     },
+    moment,
     onChange (date, dateString) {
       const start = dateString[0] // 开始时间
       const end = dateString[1] // 结束时间
@@ -82,15 +111,12 @@ export default {
         this.$emit('change', `${this.optionValue}_${start} 00:00:00_${end} 23:59:59`)
       } else {
         this.dateRangeTip = ''
+        this.handleHoverChange(false)
       }
     },
-    onMouseenter () {
-      this.dateRangeTipIsShow = this.dateRangeTip.length && true
-    },
-    onMouseleave () {
-      this.dateRangeTipIsShow = false
-    },
     onClick () {
+      this.dateRangeTip = ''
+      this.handleHoverChange(false)
       this.optionValue = this.optionOriginValue
       this.$emit('change', this.optionValue)
     }
