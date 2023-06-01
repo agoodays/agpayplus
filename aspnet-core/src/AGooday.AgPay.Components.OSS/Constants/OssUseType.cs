3b4e32719cd7435ed5fe10d916f7bef41e94db42
xiangyisheng <1697504283@qq.com>
2023-06-01 11:22:52 +0800

    OSS依赖注入容器启动器

---
+++ aspnet-core/src/AGooday.AgPay.Components.OSS/Constants/OssUseType.cs
@@ -0,0 +1,8 @@
+﻿namespace AGooday.AgPay.Components.OSS.Constants
+{
+    public class OssUseType
+    {
+        public const string LOCAL_FILE = "localFile";
+        public const string ALIYUN_OSS = "aliyunOss";
+    }
+}
---
+++ aspnet-core/src/AGooday.AgPay.Components.OSS/Extensions/NativeInjectorBootStrapper.cs
@@ -0,0 +1,48 @@
+﻿using AGooday.AgPay.Application.Interfaces;
+using AGooday.AgPay.Components.OSS.Constants;
+using AGooday.AgPay.Components.OSS.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AGooday.AgPay.Components.OSS.Extensions
+{
+    public class NativeInjectorBootStrapper
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<IOssService, AliyunOssService>();
+            services.AddScoped<IOssService, LocalFileService>(); 
+            services.AddScoped<IOssServiceFactory, OssServiceFactory>();
+        }
+    }
+
+    public interface IOssServiceFactory
+    {
+        IOssService GetService();
+    }
+
+    public class OssServiceFactory : IOssServiceFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ISysConfigService _sysConfigService;
+
+        public OssServiceFactory(IServiceProvider serviceProvider, ISysConfigService sysConfigService)
+        {
+            _serviceProvider = serviceProvider;
+            _sysConfigService = sysConfigService;
+        }
+
+        public IOssService GetService()
+        {
+            var ossUseType = _sysConfigService.GetDBOssConfig().OssUseType;
+            switch (ossUseType)
+            {
+                case OssUseType.LOCAL_FILE:
+                    return _serviceProvider.GetService<AliyunOssService>();
+                case OssUseType.ALIYUN_OSS:
+                    return _serviceProvider.GetService<LocalFileService>();
+                default:
+                    throw new Exception($"Invalid service: {ossUseType}");
+            }
+        }
+    }
+}
