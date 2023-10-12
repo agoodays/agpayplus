9b224fcdf7bae34dd4a6aebbf8606ccd93450dbb
xiangyisheng <1697504283@qq.com>
2023-10-12 11:59:38 +0800

    实现微信退款申请和查询接口

--- aspnet-core/src/AGooday.AgPay.Payment.Api/Channel/WxPay/WxPayRefundService.cs
+++ aspnet-core/src/AGooday.AgPay.Payment.Api/Channel/WxPay/WxPayRefundService.cs
@@ -1,10 +1,18 @@
 ﻿using AGooday.AgPay.Application.DataTransfer;
 using AGooday.AgPay.Application.Interfaces;
+using AGooday.AgPay.Application.Params.WxPay;
 using AGooday.AgPay.Common.Constants;
+using AGooday.AgPay.Payment.Api.Channel.WxPay.Kits;
 using AGooday.AgPay.Payment.Api.Models;
 using AGooday.AgPay.Payment.Api.RQRS.Msg;
 using AGooday.AgPay.Payment.Api.RQRS.Refund;
 using AGooday.AgPay.Payment.Api.Services;
+using SKIT.FlurlHttpClient.Wechat.TenpayV2;
+using SKIT.FlurlHttpClient.Wechat.TenpayV2.Models;
+using SKIT.FlurlHttpClient.Wechat.TenpayV3;
+using SKIT.FlurlHttpClient.Wechat.TenpayV3.Models;
+using WechatTenpayClientV2 = SKIT.FlurlHttpClient.Wechat.TenpayV2.WechatTenpayClient;
+using WechatTenpayClientV3 = SKIT.FlurlHttpClient.Wechat.TenpayV3.WechatTenpayClient;
 
 namespace AGooday.AgPay.Payment.Api.Channel.WxPay
 {
@@ -13,6 +21,8 @@ namespace AGooday.AgPay.Payment.Api.Channel.WxPay
     /// </summary>
     public class WxPayRefundService : AbstractRefundService
     {
+        private readonly ILogger<WxPayRefundService> log;
+
         public WxPayRefundService(IServiceProvider serviceProvider,
             ISysConfigService sysConfigService,
             ConfigContextQueryService configContextQueryService)
@@ -30,14 +40,178 @@ namespace AGooday.AgPay.Payment.Api.Channel.WxPay
             return null;
         }
 
+        /// <summary>
+        /// 微信退款查单接口
+        /// </summary>
+        /// <param name="refundOrder"></param>
+        /// <param name="mchAppConfigContext"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public override ChannelRetMsg Query(RefundOrderDto refundOrder, MchAppConfigContext mchAppConfigContext)
         {
-            throw new NotImplementedException();
+            try
+            {
+                ChannelRetMsg channelRetMsg = new ChannelRetMsg();
+
+                WxServiceWrapper wxServiceWrapper = _configContextQueryService.GetWxServiceWrapper(mchAppConfigContext);
+
+                if (CS.PAY_IF_VERSION.WX_V2.Equals(wxServiceWrapper.Config.ApiVersion))  // V2
+                {
+                    GetPayRefundV2Request request = new GetPayRefundV2Request();
+
+                    //放置isv信息
+                    //不是特约商户，无需放置此值
+                    if (mchAppConfigContext.IsIsvSubMch())
+                    {
+                        WxPayIsvSubMchParams isvsubMchParams =
+                            (WxPayIsvSubMchParams)_configContextQueryService.QueryIsvSubMchParams(mchAppConfigContext.MchNo, mchAppConfigContext.AppId, CS.IF_CODE.WXPAY);
+
+                        request.SubMerchantId = isvsubMchParams.SubMchId;
+                        request.SubAppId = isvsubMchParams.SubMchAppId;
+                    }
+
+                    request.OutRefundNumber = refundOrder.RefundOrderId; // 退款单号
+                    var client = (WechatTenpayClientV2)wxServiceWrapper.Client;
+                    var result = client.ExecuteGetPayRefundV2Async(request).Result;
+
+                    if ("SUCCESS".Equals(result.ResultCode))  // 退款成功
+                    {
+                        channelRetMsg.ChannelState = ChannelState.CONFIRM_SUCCESS;
+                    }
+                    else
+                    {
+                        channelRetMsg.ChannelState = ChannelState.WAITING;
+                        channelRetMsg.ChannelErrMsg = result.ReturnMessage;
+                    }
+                }
+                else if (CS.PAY_IF_VERSION.WX_V3.Equals(wxServiceWrapper.Config.ApiVersion))  // V3
+                {
+                    var client = (WechatTenpayClientV3)wxServiceWrapper.Client;
+                    GetRefundDomesticRefundByOutRefundNumberRequest request = new GetRefundDomesticRefundByOutRefundNumberRequest();
+                    request.OutRefundNumber = refundOrder.RefundOrderId;
+                    if (mchAppConfigContext.IsIsvSubMch())
+                    {
+                        WxPayIsvSubMchParams isvsubMchParams = (WxPayIsvSubMchParams)_configContextQueryService.QueryIsvSubMchParams(mchAppConfigContext.MchNo, mchAppConfigContext.AppId, GetIfCode());
+                        request.SubMerchantId = isvsubMchParams.SubMchId;
+                    }
+                    var result = client.ExecuteGetRefundDomesticRefundByOutRefundNumberAsync(request).Result;
+                    if (result.IsSuccessful())  // 退款成功
+                    {
+                        channelRetMsg.ChannelState = ChannelState.CONFIRM_SUCCESS;
+                    }
+                    else
+                    {
+                        channelRetMsg.ChannelState = ChannelState.WAITING;
+                        channelRetMsg.ChannelErrMsg = result.ErrorMessage;
+                    }
+                }
+                return channelRetMsg;
+            }
+            catch (Exception e)
+            {
+                log.LogError(e, "微信退款查询Exception异常: ");
+                return ChannelRetMsg.SysError(e.Message);
+            }
         }
 
+        /// <summary>
+        /// 微信退款接口
+        /// </summary>
+        /// <param name="bizRQ"></param>
+        /// <param name="refundOrder"></param>
+        /// <param name="payOrder"></param>
+        /// <param name="mchAppConfigContext"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public override ChannelRetMsg Refund(RefundOrderRQ bizRQ, RefundOrderDto refundOrder, PayOrderDto payOrder, MchAppConfigContext mchAppConfigContext)
         {
-            throw new NotImplementedException();
+            try
+            {
+                ChannelRetMsg channelRetMsg = new ChannelRetMsg();
+                WxServiceWrapper wxServiceWrapper = _configContextQueryService.GetWxServiceWrapper(mchAppConfigContext);
+
+                if (CS.PAY_IF_VERSION.WX_V2.Equals(wxServiceWrapper.Config.ApiVersion)) // V2
+                {
+                    CreatePayRefundV2Request request = new CreatePayRefundV2Request();
+
+                    //放置isv信息
+                    //不是特约商户，无需放置此值
+                    if (mchAppConfigContext.IsIsvSubMch())
+                    {
+                        WxPayIsvSubMchParams isvsubMchParams =
+                            (WxPayIsvSubMchParams)_configContextQueryService.QueryIsvSubMchParams(mchAppConfigContext.MchNo, mchAppConfigContext.AppId, CS.IF_CODE.WXPAY);
+
+                        request.SubMerchantId = isvsubMchParams.SubMchId;
+                        request.SubAppId = isvsubMchParams.SubMchAppId;
+                    }
+
+                    request.OutTradeNumber = payOrder.PayOrderId; // 商户订单号
+                    request.OutRefundNumber = refundOrder.RefundOrderId; // 退款单号
+                    request.TotalFee = (int)payOrder.Amount; // 订单总金额
+                    request.RefundFee = (int)refundOrder.RefundAmount; // 退款金额
+                    request.NotifyUrl = GetNotifyUrl(refundOrder.RefundOrderId); // 回调url
+
+                    var client = (WechatTenpayClientV2)wxServiceWrapper.Client;
+                    var result = client.ExecuteCreatePayRefundV2Async(request).Result;
+
+                    if (result.IsSuccessful()) // 退款发起成功,结果主动查询
+                    {
+                        channelRetMsg.ChannelState = ChannelState.WAITING;
+                        channelRetMsg.ChannelOrderId = result.RefundId;
+                    }
+                    else
+                    {
+                        channelRetMsg.ChannelState = ChannelState.CONFIRM_FAIL;
+                        channelRetMsg.ChannelErrCode = result.ErrorCode;
+                        channelRetMsg.ChannelErrMsg = WxPayKit.AppendErrMsg(result.ReturnMessage, result.ErrorCodeDescription);
+                    }
+                }
+                else if (CS.PAY_IF_VERSION.WX_V3.Equals(wxServiceWrapper.Config.ApiVersion)) // V3
+                {
+                    CreateRefundDomesticRefundRequest request = new CreateRefundDomesticRefundRequest();
+                    request.OutTradeNumber = refundOrder.PayOrderId; // 订单号
+                    request.OutRefundNumber = refundOrder.RefundOrderId; // 退款订单号
+                    request.NotifyUrl = GetNotifyUrl(refundOrder.RefundOrderId); // 回调地址
+
+                    request.Amount.Refund = (int)refundOrder.RefundAmount; // 退款金额
+                    request.Amount.Total = (int)payOrder.Amount; // 订单总金额
+                    request.Amount.Currency = "CNY"; // 币种
+
+                    if (mchAppConfigContext.IsIsvSubMch()) // 特约商户
+                    {
+                        WxPayIsvSubMchParams isvsubMchParams = (WxPayIsvSubMchParams)_configContextQueryService.QueryIsvSubMchParams(mchAppConfigContext.MchNo, mchAppConfigContext.AppId, GetIfCode());
+                        request.SubMerchantId = isvsubMchParams.SubMchId;
+                    }
+
+                    var client = (WechatTenpayClientV3)wxServiceWrapper.Client;
+                    var result = client.ExecuteCreateRefundDomesticRefundAsync(request).Result;
+                    string status = result.Status;
+                    if ("SUCCESS".Equals(status)) // 退款成功
+                    {
+                        string refundId = result.RefundId;
+                        channelRetMsg.ChannelState = ChannelState.CONFIRM_SUCCESS;
+                        channelRetMsg.ChannelOrderId = refundId;
+                    }
+                    else if ("PROCESSING".Equals(status)) // 退款处理中
+                    {
+                        string refundId = result.RefundId;
+                        channelRetMsg.ChannelState = ChannelState.WAITING;
+                        channelRetMsg.ChannelOrderId = refundId;
+                    }
+                    else
+                    {
+                        channelRetMsg.ChannelState = ChannelState.CONFIRM_FAIL;
+                        channelRetMsg.ChannelErrMsg = result.ErrorMessage;
+                    }
+                }
+
+                return channelRetMsg;
+            }
+            catch (Exception e)
+            {
+                log.LogError(e, "微信退款Exception异常: ");
+                return ChannelRetMsg.SysError(e.Message);
+            }
         }
     }
 }
