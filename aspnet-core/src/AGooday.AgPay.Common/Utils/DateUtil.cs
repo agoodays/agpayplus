602f1ad9ffcaff6e3d11c0a3b68f674c9a86690b
xiangyisheng <1697504283@qq.com>
2024-03-07 10:29:34 +0800

    DateUtil新增当月和上月

--- aspnet-core/src/AGooday.AgPay.Common/Utils/DateUtil.cs
+++ aspnet-core/src/AGooday.AgPay.Common/Utils/DateUtil.cs
@@ -4,6 +4,8 @@
     {
         public const string TODAY = "today";
         public const string YESTERDAY = "yesterday";
+        public const string CURR_MONTH = "currMonth";
+        public const string PREV_MONTH = "prevMonth";
         public const string NEAR2NOW = "near2now";
         public const string CUSTOM_DATE_TIME = "customDateTime";
 
@@ -11,21 +13,33 @@
         {
             queryDateRange ??= string.Empty;
             createdStart = null; createdEnd = null;
+            var today = DateTime.Today;
             if (queryDateRange.Equals(TODAY))
             {
-                createdStart = DateTime.Today.ToString("yyyy-MM-dd HH:mm:ss");
-                createdEnd = DateTime.Today.AddDays(1).AddSeconds(-1).ToString("yyyy-MM-dd HH:mm:ss");
+                createdStart = today.ToString("yyyy-MM-dd HH:mm:ss");
+                createdEnd = today.AddDays(1).AddSeconds(-1).ToString("yyyy-MM-dd HH:mm:ss");
             }
             if (queryDateRange.Equals(YESTERDAY))
             {
-                createdStart = DateTime.Today.AddDays(-1).ToString("yyyy-MM-dd HH:mm:ss");
-                createdEnd = DateTime.Today.AddSeconds(-1).ToString("yyyy-MM-dd HH:mm:ss");
+                createdStart = today.AddDays(-1).ToString("yyyy-MM-dd HH:mm:ss");
+                createdEnd = today.AddSeconds(-1).ToString("yyyy-MM-dd HH:mm:ss");
+            }
+            if (queryDateRange.Equals(CURR_MONTH))
+            {
+                createdStart = today.AddDays(1 - today.Day).ToString("yyyy-MM-dd HH:mm:ss");
+                createdEnd = today.AddDays(1).AddSeconds(-1).ToString("yyyy-MM-dd HH:mm:ss");
+            }
+            if (queryDateRange.Equals(PREV_MONTH))
+            {
+                var firstDayOfMonth = today.AddDays(1 - today.Day);
+                createdStart = firstDayOfMonth.AddMonths(-1).ToString("yyyy-MM-dd HH:mm:ss");
+                createdEnd = firstDayOfMonth.AddSeconds(-1).ToString("yyyy-MM-dd HH:mm:ss");
             }
             if (queryDateRange.StartsWith(NEAR2NOW))
             {
                 int day = Convert.ToInt32(queryDateRange.Split("_")[1]);
-                createdStart = DateTime.Today.AddDays(-(day - 1)).ToString("yyyy-MM-dd HH:mm:ss");
-                createdEnd = DateTime.Today.AddDays(1).AddSeconds(-1).ToString("yyyy-MM-dd HH:mm:ss");
+                createdStart = today.AddDays(-(day - 1)).ToString("yyyy-MM-dd HH:mm:ss");
+                createdEnd = today.AddDays(1).AddSeconds(-1).ToString("yyyy-MM-dd HH:mm:ss");
             }
             if (queryDateRange.StartsWith(CUSTOM_DATE_TIME))
             {
