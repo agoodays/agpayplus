6491c17f070d33de6652e5fc381374128cdc3944
xiangyisheng <1697504283@qq.com>
2024-03-13 11:08:21 +0800

    AES加密解密单元测试

---
+++ aspnet-core/test/AGooday.AgPay.Common.UnitTests/AgPayUtilTest.cs
@@ -0,0 +1,22 @@
+using AGooday.AgPay.Common.Utils;
+
+namespace AGooday.AgPay.Common.UnitTests
+{
+    [TestClass]
+    public class AgPayUtilTest
+    {
+        [TestMethod]
+        public void AgPayAesTest()
+        {
+            var data = "65d86708e4b0c884420d9b9a";
+            var cipherText = AgPayUtil.AesEncode(data);
+            var plainText = AgPayUtil.AesDecode(cipherText);
+            var _cipherText = "4608adcbef5a3e18f60a4c1a92bef61db4fff4d1e1b250b8775b1c7317605836";
+            var _plainText = AgPayUtil.AesDecode(_cipherText);
+
+            Assert.AreEqual(cipherText, _cipherText, true);
+            Assert.AreEqual(data, plainText);
+            Assert.AreEqual(data, _plainText);
+        }
+    }
+}
