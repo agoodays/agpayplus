934869b26ae45a849827b52052f2d0e0e6144451
Xiang YiSheng <1697504283@qq.com>
2024-04-04 23:27:15 +0800

    调整服务实现

--- aspnet-core/src/AGooday.AgPay.Application/Interfaces/IAgPayService.cs
+++ aspnet-core/src/AGooday.AgPay.Application/Interfaces/IAgPayService.cs
@@ -1,15 +1,5 @@
 ﻿namespace AGooday.AgPay.Application.Interfaces
 {
-    public interface IAgPayService<TDto> : IDisposable
-        where TDto : class
-    {
-        bool Add(TDto dto);
-        bool Remove<TPrimaryKey>(TPrimaryKey id);
-        bool Update(TDto dto);
-        TDto GetById<TPrimaryKey>(TPrimaryKey id);
-        Task<TDto> GetByIdAsync<TPrimaryKey>(TPrimaryKey recordId);
-        IEnumerable<TDto> GetAll();
-    }
     public interface IAgPayService<TDto, TPrimaryKey> : IDisposable
         where TDto : class
         where TPrimaryKey : struct
@@ -19,6 +9,16 @@
         bool Update(TDto dto);
         TDto GetById(TPrimaryKey id);
         Task<TDto> GetByIdAsync(TPrimaryKey recordId);
+        IEnumerable<TDto> GetAll();
+    }
+    public interface IAgPayService<TDto> : IDisposable
+        where TDto : class
+    {
+        bool Add(TDto dto);
+        bool Remove<TPrimaryKey>(TPrimaryKey id);
+        bool Update(TDto dto);
+        TDto GetById<TPrimaryKey>(TPrimaryKey id);
+        Task<TDto> GetByIdAsync<TPrimaryKey>(TPrimaryKey recordId);
         IEnumerable<TDto> GetAll();
     }
 }
--- aspnet-core/src/AGooday.AgPay.Application/Services/AgPayService.cs
+++ aspnet-core/src/AGooday.AgPay.Application/Services/AgPayService.cs
@@ -8,75 +8,6 @@ namespace AGooday.AgPay.Application.Services
     /// <summary>
     /// 账户帐单表 服务实现类
     /// </summary>
-    public abstract class AgPayService<TDto, TEntity> : IAgPayService<TDto>
-        where TDto : class
-        where TEntity : class
-    {
-        // 注意这里是要IoC依赖注入的，还没有实现
-        protected readonly IAgPayRepository<TEntity> _agPayRepository;
-        // 用来进行DTO
-        protected readonly IMapper _mapper;
-        // 中介者 总线
-        protected readonly IMediatorHandler Bus;
-
-        public AgPayService(IMapper mapper, IMediatorHandler bus,
-            IAgPayRepository<TEntity> agPayRepository)
-        {
-            _mapper = mapper;
-            Bus = bus;
-            _agPayRepository = agPayRepository;
-        }
-
-        public void Dispose()
-        {
-            GC.SuppressFinalize(this);
-        }
-
-        public virtual bool Add(TDto dto)
-        {
-            var entity = _mapper.Map<TEntity>(dto);
-            _agPayRepository.Add(entity);
-            var result = _agPayRepository.SaveChanges(out int _);
-            return result;
-        }
-
-        public virtual bool Remove<TPrimaryKey>(TPrimaryKey id)
-        {
-            _agPayRepository.Remove(id);
-            return _agPayRepository.SaveChanges(out int _);
-        }
-
-        public virtual bool Update(TDto dto)
-        {
-            var entity = _mapper.Map<TEntity>(dto);
-            _agPayRepository.Update(entity);
-            return _agPayRepository.SaveChanges(out int _);
-        }
-
-        public virtual TDto GetById<TPrimaryKey>(TPrimaryKey id)
-        {
-            var entity = _agPayRepository.GetById(id);
-            var dto = _mapper.Map<TDto>(entity);
-            return dto;
-        }
-
-        public async Task<TDto> GetByIdAsync<TPrimaryKey>(TPrimaryKey id)
-        {
-            var entity = await _agPayRepository.GetByIdAsync(id);
-            var dto = _mapper.Map<TDto>(entity);
-            return dto;
-        }
-
-        public virtual IEnumerable<TDto> GetAll()
-        {
-            //第一种写法 Map
-            var sysUsers = _agPayRepository.GetAll();
-            return _mapper.Map<IEnumerable<TDto>>(sysUsers);
-
-            //第二种写法 ProjectTo
-            //return (_agPayRepository.GetAll()).ProjectTo<TDto>(_mapper.ConfigurationProvider);
-        }
-    }
     public abstract class AgPayService<TDto, TEntity, TPrimaryKey> : IAgPayService<TDto, TPrimaryKey>
         where TDto : class
         where TEntity : class
@@ -130,7 +61,7 @@ namespace AGooday.AgPay.Application.Services
             return dto;
         }
 
-        public async Task<TDto> GetByIdAsync(TPrimaryKey id)
+        public virtual async Task<TDto> GetByIdAsync(TPrimaryKey id)
         {
             var entity = await _agPayRepository.GetByIdAsync(id);
             var dto = _mapper.Map<TDto>(entity);
@@ -141,6 +72,75 @@ namespace AGooday.AgPay.Application.Services
         {
             var entitys = _agPayRepository.GetAll();
             return _mapper.Map<IEnumerable<TDto>>(entitys);
+        }
+    }
+    public abstract class AgPayService<TDto, TEntity> : IAgPayService<TDto>
+        where TDto : class
+        where TEntity : class
+    {
+        // 注意这里是要IoC依赖注入的，还没有实现
+        protected readonly IAgPayRepository<TEntity> _agPayRepository;
+        // 用来进行DTO
+        protected readonly IMapper _mapper;
+        // 中介者 总线
+        protected readonly IMediatorHandler Bus;
+
+        public AgPayService(IMapper mapper, IMediatorHandler bus,
+            IAgPayRepository<TEntity> agPayRepository)
+        {
+            _mapper = mapper;
+            Bus = bus;
+            _agPayRepository = agPayRepository;
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+
+        public virtual bool Add(TDto dto)
+        {
+            var entity = _mapper.Map<TEntity>(dto);
+            _agPayRepository.Add(entity);
+            var result = _agPayRepository.SaveChanges(out int _);
+            return result;
+        }
+
+        public virtual bool Remove<TPrimaryKey>(TPrimaryKey id)
+        {
+            _agPayRepository.Remove(id);
+            return _agPayRepository.SaveChanges(out int _);
+        }
+
+        public virtual bool Update(TDto dto)
+        {
+            var entity = _mapper.Map<TEntity>(dto);
+            _agPayRepository.Update(entity);
+            return _agPayRepository.SaveChanges(out int _);
+        }
+
+        public virtual TDto GetById<TPrimaryKey>(TPrimaryKey id)
+        {
+            var entity = _agPayRepository.GetById(id);
+            var dto = _mapper.Map<TDto>(entity);
+            return dto;
+        }
+
+        public virtual async Task<TDto> GetByIdAsync<TPrimaryKey>(TPrimaryKey id)
+        {
+            var entity = await _agPayRepository.GetByIdAsync(id);
+            var dto = _mapper.Map<TDto>(entity);
+            return dto;
+        }
+
+        public virtual IEnumerable<TDto> GetAll()
+        {
+            //第一种写法 Map
+            var sysUsers = _agPayRepository.GetAll();
+            return _mapper.Map<IEnumerable<TDto>>(sysUsers);
+
+            //第二种写法 ProjectTo
+            //return (_agPayRepository.GetAll()).ProjectTo<TDto>(_mapper.ConfigurationProvider);
         }
     }
 }
