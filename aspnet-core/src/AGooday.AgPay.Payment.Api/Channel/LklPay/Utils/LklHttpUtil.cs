09b78b9c9c00ac4cef980a8c19a11d0893c086f4
xiangyisheng <1697504283@qq.com>
2024-04-07 18:06:19 +0800

    拉卡拉支付接入准备

---
+++ aspnet-core/src/AGooday.AgPay.Payment.Api/Channel/LklPay/Utils/LklHttpUtil.cs
@@ -0,0 +1,43 @@
+﻿using AGooday.AgPay.Common.Utils;
+using AGooday.AgPay.Payment.Api.Exceptions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AGooday.AgPay.Payment.Api.Channel.LklPay.Utils
+{
+    public class LklHttpUtil
+    {
+        private static readonly string DEFAULT_CHARSET = "UTF-8";
+        private static readonly int DEFAULT_TIMEOUT = 60; // 60 秒超时
+
+        public static string DoPostJson(string url, string appId, string serialNo,string privateKey, JObject reqParams, out Dictionary<string, string> headers)
+        {
+            headers = null;
+            var client = new AgHttpClient(DEFAULT_TIMEOUT, DEFAULT_CHARSET);
+            var authorization = LklSignUtil.GetAuthorizationHeader(appId, serialNo, reqParams.ToString(), privateKey);
+            var request = new AgHttpClient.Request()
+            {
+                Url = url,
+                Method = "POST",
+                Content = JsonConvert.SerializeObject(reqParams),
+                ContentType = "application/json",
+                Headers = new Dictionary<string, string> { { "Authorization", authorization } }
+            };
+            try
+            {
+                var response = client.Send(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = response.Content;
+                    headers = response.Headers;
+                    return result;
+                }
+                return null;
+            }
+            catch (Exception e)
+            {
+                throw ChannelException.SysError(e.Message);
+            }
+        }
+    }
+}
---
+++ aspnet-core/src/AGooday.AgPay.Payment.Api/Channel/LklPay/Utils/LklSignUtil.cs
@@ -0,0 +1,56 @@
+﻿using AGooday.AgPay.Common.Utils;
+
+namespace AGooday.AgPay.Payment.Api.Channel.LklPay.Utils
+{
+    public class LklSignUtil
+    {
+        public static string GetAuthorizationHeader(string appId, string serialNo, string strBody, string privateKey)
+        {
+            string timeStamp = DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
+            string nonceStr = GetNonceStr(12);
+            string reqbody = GenSigned(appId, serialNo, strBody, timeStamp, nonceStr);
+            string signature = Sign(reqbody, privateKey);
+            string auth = "LKLAPI-SHA256withRSA appid=\"" + appId + "\",serial_no=\"" + serialNo + "\",timestamp=\"" + timeStamp + "\",nonce_str=\"" + nonceStr + "\",signature=\"" + signature + "\"";
+            return auth;
+        }
+
+        public static bool Verify(Dictionary<string, string> headers, string appId, string data, string publicKey)
+        {
+            headers.TryGetValue("Lklapi-Serial", out string serialNo);
+            headers.TryGetValue("Lklapi-Timestamp", out string timeStamp);
+            headers.TryGetValue("Lklapi-Nonce", out string nonceStr);
+            headers.TryGetValue("Lklapi-Signature", out string signature);
+            string reqbody = GenSigned(appId, serialNo, data, timeStamp, nonceStr);
+            var flag = RsaUtil.Verify(reqbody, publicKey, signature, signType: "RSA2");
+            return flag;
+        }
+
+        private static string Sign(string data, string privateKey)
+        {
+            var sign = RsaUtil.Sign(data, privateKey, signType: "RSA2");
+            return sign;
+        }
+
+        private static string GenSigned(string appId, string serialNo, string strBody, string timeStamp, string nonceStr)
+        {
+            return appId + "\n" + serialNo + "\n" + timeStamp + "\n" + nonceStr + "\n" + strBody + "\n";
+        }
+
+        /// <summary>
+        /// 生成随机12个字符
+        /// </summary>
+        /// <param name="lenth"></param>
+        /// <returns></returns>
+        private static string GetNonceStr(int lenth)
+        {
+            string result = "";
+            Random rand = new Random();
+            char[] strmap = "0123456789abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            for (int i = 0; i < lenth; i++)
+            {
+                result += strmap[rand.Next() % strmap.Length];
+            }
+            return result;
+        }
+    }
+}
