d3db6893f43e2df974a56433265a402b152504b7
xiangyisheng <1697504283@qq.com>
2024-04-29 13:21:20 +0800

    修改支付宝沙箱环境请求地址

--- aspnet-core/src/AGooday.AgPay.Application/Params/AliPay/AliPayConfig.cs
+++ aspnet-core/src/AGooday.AgPay.Application/Params/AliPay/AliPayConfig.cs
@@ -14,11 +14,11 @@
         public const string SANDBOX_SERVER_URL = "https://openapi-sandbox.dl.alipaydev.com/gateway.do";
 
         public const string PROD_OAUTH_URL = "https://openauth.alipay.com/oauth2/publicAppAuthorize.htm?app_id={0}&scope=auth_base&state=&redirect_uri={1}";
-        public const string SANDBOX_OAUTH_URL = "https://openapi-sandbox.dl.alipaydev.com/oauth2/publicAppAuthorize.htm?app_id={0}&scope=auth_base&state=&redirect_uri={1}";
+        public const string SANDBOX_OAUTH_URL = "https://openauth-sandbox.dl.alipaydev.com/oauth2/publicAppAuthorize.htm?app_id={0}&scope=auth_base&state=&redirect_uri={1}";
 
         /** isv获取授权商户URL地址 **/
         public const string PROD_APP_TO_APP_AUTH_URL = "https://openauth.alipay.com/oauth2/appToAppAuth.htm?app_id={0}&redirect_uri={1}&state={2}";
-        public const string SANDBOX_APP_TO_APP_AUTH_URL = "https://openapi-sandbox.dl.alipaydev.com/oauth2/appToAppAuth.htm?app_id={0}&redirect_uri={1}&state={2}";
+        public const string SANDBOX_APP_TO_APP_AUTH_URL = "https://openauth-sandbox.dl.alipaydev.com/oauth2/appToAppAuth.htm?app_id={0}&redirect_uri={1}&state={2}";
 
 
         public const string FORMAT = "json";
