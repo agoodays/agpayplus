90050c483f47aaadc88789e0bdaed69ef9aa506c
xiangyisheng <1697504283@qq.com>
2024-05-24 18:30:02 +0800

    Vue3前端项目搭建

--- ant-design-vue3/agpay-ui-manager/src/router/index.js
+++ ant-design-vue3/agpay-ui-manager/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import UserLayout from '/@/layouts/user-layout.vue'
 import AgLayout from '/@/layouts/index.vue'
+import nProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 const routes = [
     // 定义你的路由配置
@@ -23,4 +25,33 @@ const routes = [
 export const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+// 获取到第一个uri (递归查找)
+function getOneUri (item) {
+    let result = ''
+    for (let i = 0; i < item.length; i++) {
+        if (item[i].menuUri && item[i].entType === 'ML') {
+            return item[i].menuUri
+        }
+
+        if (item[i].children) {
+            result = getOneUri(item[i].children)
+            if (result) {
+                return result
+            }
+        }
+    }
+    return result
+}
+
+router.beforeEach((to, from, next) => {
+    // 进度条开启
+    nProgress.start();
+
+
+})
+
+router.afterEach(() => {
+    nProgress.done();
 });
