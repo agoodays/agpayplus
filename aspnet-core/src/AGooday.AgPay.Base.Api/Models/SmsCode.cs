51007b4e60703362591c6cb5217fafde3d8eb3fa
Xiang YiSheng <1697504283@qq.com>
2026-02-02 13:18:25 +0800

    修复Bug

--- aspnet-core/src/AGooday.AgPay.Base.Api/Models/SmsCode.cs
+++ aspnet-core/src/AGooday.AgPay.Base.Api/Models/SmsCode.cs
@@ -7,8 +7,8 @@
         /// </summary>
         public string Phone { get; set; }
         /// <summary>
-        /// 短信验证码类型 1.注册 2.登录 3.修改密码 4.设置支付密码 5.修改手机号
+        /// 短信验证码类型
         /// </summary>
-        public byte SmsType { get; set; }
+        public string SmsType { get; set; }
     }
 }
